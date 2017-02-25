b405bc9b7fd47544e9b88782bc15619d94b3a461
AIC Web Design <pwm@preservedword.com>
2017-02-25 16:01:28 -0500

    API-27: Tweaked access limitation

--- app/Http/Middleware/ApiAccess.php
+++ app/Http/Middleware/ApiAccess.php
@@ -20,9 +20,10 @@ class ApiAccess
      * @return mixed
      */
     public function handle($request, Closure $next) {
-        $host = (array_key_exists('REMOTE_HOST', $_SERVER)) ? $_SERVER['REMOTE_HOST'] : NULL;
-        
-        $IP = IpAccess::findOrCreateByIpOrDomain($_SERVER['REMOTE_ADDR'], $host);
+        $host = (array_key_exists('HTTP_REFERER', $_SERVER)) ? $_SERVER['HTTP_REFERER'] : NULL;
+        $ip = gethostbyname($host);
+        $ip = ($ip == $host) ? $_SERVER['REMOTE_ADDR'] : $ip;
+        $IP = IpAccess::findOrCreateByIpOrDomain($ip, $host);
         
         if(!$IP->incrementDailyHits()) {
             $response = new \stdClass;
