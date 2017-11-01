334c1f3bd3db963b36cecba4606c2a22d8579f35
AIC Web Design <pwm@preservedword.com>
2017-10-31 21:03:51 -0400

    CLIENT-29: Fixed slow middleware

--- app/Http/Middleware/ApiAccess.php
+++ app/Http/Middleware/ApiAccess.php
@@ -21,19 +21,20 @@ class ApiAccess
      */
     public function handle($request, Closure $next) {
         $host = (array_key_exists('HTTP_REFERER', $_SERVER)) ? $_SERVER['HTTP_REFERER'] : NULL;
-        $ip = gethostbyname($host);
-        $ip = ($ip == $host) ? $_SERVER['REMOTE_ADDR'] : $ip;
+        // $ip = gethostbyname($host); // Cannot do this - as this is for IP v4 ONLY
+        // $ip = ($ip == $host) ? $_SERVER['REMOTE_ADDR'] : $ip;
+        $ip = ($host) ? $_SERVER['REMOTE_ADDR'] : NULL;
         $IP = IpAccess::findOrCreateByIpOrDomain($ip, $host);
-        
+
         if(!$IP->incrementDailyHits()) {
             $response = new \stdClass;
             $response->errors = array(trans('errors.hit_limit_reached'));
-            
+
             return (new Response(json_encode($response), 500))
             -> header('Content-Type', 'application/json; charset=utf-8')
             -> header('Access-Control-Allow-Origin', '*');
         }
-        
+
         return $next($request);
     }
 }
