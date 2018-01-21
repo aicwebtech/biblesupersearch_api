3565b2a96d7bba6d76fe9865183606e6bb63bc7a
AIC Web Design <pwm@preservedword.com>
2018-01-20 20:50:06 -0500

    CLIENT-29: Added terms of service and privacy polity to API docs

---
+++ resources/views/docs/tos.php
@@ -0,0 +1,35 @@
+<?php
+    // todo: check for customer provided TOS
+?>
+
+<h3>Terms of Service</h3>
+
+By using the Bible SuperSearch API ("service", "API"), you agree to the terms of this agreement between yourself and
+Preserved Word Ministries ("we", "our", "us") (Bible SuperSearch is a ministry of Preserved Word Ministries).<br /><br />
+
+This service is made available in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. &nbsp;<br /><br />
+
+We reserve the right to limit access to the service. &nbsp;We reserve the right to discontinue the service at any time. &nbsp;<br /><br />
+
+Bible SuperSearch is free for NON-COMMERCIAL usage. &nbsp;Any commercial usage requires our permission.
+
+<h4>Support</h4>
+
+While we offer free access to the API, support will require the the purchase of a support license. &nbsp;
+We can only offer support for the API located at https://api.biblesupersearch.com.  &nbsp; We are unable
+to provide support for any of our sandbox APIs, nor for instances of our API hosted by a third party. &nbsp;
+While this API can be downloaded and installed from our GIT repository, we cannot provide support for this at this time.
+
+<h4>Returns</h4>
+
+All sales are final on support licenses, software, and software enhancements.
+
+<h4>Prohibited Usage</h4>
+<ul>
+    <li>Attempting to circumvent daily access limit.</li>
+    <li>Reselling access to the API, or acting a relay or proxy between the API and a third party.</li>
+    <li>Use for any other commercial purpose, such as inclusion behind a paid access login.</li>
+    <li>Providing API access keys, if any, to a third party.</li>
+    <li>Using the API to demean, vilify or blaspheme God, Jesus Christ, the Holy Spirit or the Bible itself.</li>
+</ul>
