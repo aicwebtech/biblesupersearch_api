8fb8e238380455863ade8887a244451e31b6251e
AIC Web Design <pwm@preservedword.com>
2018-01-27 16:40:13 -0500

    CLIENT-29: Revised TOS / Privacy policy

--- resources/views/docs/privacy.php
+++ resources/views/docs/privacy.php
@@ -5,16 +5,15 @@
 <h3>Privacy Policy</h3>
 
 Preserved Word Ministries ("we") value your ("customer") privacy and the privacy of the visitors to your website ("end users").
-We track the number of times your website accesses the API for each day, to prevent over-usage. <br /><br />
+We track the number of times a customer's website accesses the API for each day, to prevent over-usage. <br /><br />
 
-We do not store any user-identifiable data on end users.  &nbsp; We do store <u>anonymous</u> form data;
+The API does not store any user-identifiable data on end users. &nbsp; We do store <u>anonymous</u> form data;
 this is needed to generate short URLs that are used to enhance the user's experience, including
 the ability to share a link to a search, and allow the browser's back / forward buttons to work
-as expected, even within the context of a JavaScript application such as our client software.<br /><br />
+as expected within the context of a JavaScript application such as our client software.<br /><br />
 
-<br />If you have any questions or concerns, please contact us at <br /><br />
+The API does not collect or store credit card data or other sensitive information. &nbsp; All official client software transmit form data to the API
+via HTTPS.<br /><br />
 
-Preserved Word Ministries<br />
-P.O. Box 128, Byron Center, MI USA 49315<br />
-pwm AT preservedword DOT com<br />
-1.616.730.2920
+<br />If you have any questions or concerns, please contact us at
+<a href='https://www.biblesupersearch.com/contact/'>https://www.biblesupersearch.com/contact/</a>
--- resources/views/docs/tos.php
+++ resources/views/docs/tos.php
@@ -4,32 +4,59 @@
 
 <h3>Terms of Service</h3>
 
-By using the Bible SuperSearch API ("service", "API"), you agree to the terms of this agreement between yourself and
+By using the Bible SuperSearch API ("service", "API"), you agree to the terms of this agreement between yourself ("customer") and
 Preserved Word Ministries ("we", "our", "us") (Bible SuperSearch is a ministry of Preserved Word Ministries).<br /><br />
-
-This service is made available in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. &nbsp;<br /><br />
 
 We reserve the right to limit access to the service. &nbsp;We reserve the right to discontinue the service at any time. &nbsp;<br /><br />
 
-Bible SuperSearch is free for NON-COMMERCIAL usage. &nbsp;Any commercial usage requires our permission.
+The Bible SuperSearch API and client software are licensed for NON-COMMERCIAL usage ONLY. &nbsp;
+Any commercial usage requires our prior expressed written permission.
 
 <h4>Support</h4>
-
 While we offer free access to the API, support will require the the purchase of a support license. &nbsp;
 We can only offer support for the API located at https://api.biblesupersearch.com.  &nbsp; We are unable
 to provide support for any of our sandbox APIs, nor for instances of our API hosted by a third party. &nbsp;
-While this API can be downloaded and installed from our GIT repository, we cannot provide support for this at this time.
+While the code for this API is Open Source and can be downloaded and installed from our GIT repository, we cannot provide support for this at this time.
 
 <h4>Returns</h4>
 
 All sales are final on support licenses, software, and software enhancements.
 
+<h4>No Warranty</h4>
+This service is made available in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+<h4>Limitation of Liability</h4>
+In no event shall Preserved Word Ministries, nor any of its officers, directors and employees, be liable to you for anything arising out of or in any way connected with
+your use of this service, whether such liability is under contract, tort or otherwise, and Preserved Word Ministries, including its officers, directors and employees shall
+not be liable for any indirect, consequential or special liability arising out of or in any way related to your use of this service.
+
+<h4>Indemnification</h4>
+You hereby indemnify to the fullest extent Preserved Word Ministries from and against any and all liabilities, costs, demands, causes of action, damages and
+expenses (including reasonable attorney’s fees) arising out of or in any way related to your breach of any of the provisions of these Terms.
+
+
 <h4>Prohibited Usage</h4>
+
+We reserve the right to terminate a customer's access to the service for any usage we deem to be an abuse,
+including but not limited to the following: <br />
 <ul>
-    <li>Attempting to circumvent daily access limit.</li>
-    <li>Reselling access to the API, or acting a relay or proxy between the API and a third party.</li>
-    <li>Use for any other commercial purpose, such as inclusion behind a paid access login.</li>
+    <li>Attempting to circumvent the daily access limit.</li>
+    <li>Reselling access to the service, or acting a relay or proxy between the API and a third party.</li>
+    <li>Use for any commercial purpose (such as inclusion behind a paid access login), without our prior expressed written permission.</li>
     <li>Providing API access keys, if any, to a third party.</li>
-    <li>Using the API to demean, vilify or blaspheme God, Jesus Christ, the Holy Spirit or the Bible itself.</li>
+    <li>Using the service in any way to impact the access of other users.</li>
+    <li>Using the service to demean, vilify or blaspheme God, Jesus Christ, the Holy Spirit or the Bible itself.</li>
 </ul>
+
+<h4>Severability</h4>
+If any provision of these Terms is found to be unenforceable or invalid under any applicable law, such unenforceability or invalidity shall not render these
+Terms unenforceable or invalid as a whole, and such provisions shall be deleted without affecting the remaining provisions herein.
+
+<h4>Variation of Terms</h4>
+Preserved Word Ministries is permitted to revise these Terms at any time as it sees fit, and by using this service you are expected to review such Terms on a regular basis
+to ensure you understand all terms and conditions governing use of this service.
+
+<h4>Governing Law and Jurisdiction</h4>
+This agreement shall be governed pursuant to the laws of the state of Michigan.
+
