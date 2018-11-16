16f7a56a71269de5d5cacc20d6295d437f862c37
AIC Web Design <pwm@preservedword.com>
2018-11-15 21:40:03 -0500

    EMAIL FIX: test 5

--- resources/views/vendor/mail/markdown/message.blade.php
+++ resources/views/vendor/mail/markdown/message.blade.php
@@ -9,14 +9,6 @@
     {{-- Body --}}
     {{ $slot }}
 
-    {{-- Subcopy --}}
-    @isset($subcopy)
-        @slot('subcopy')
-            @component('mail::subcopy')
-                {{ $subcopy }}
-            @endcomponent
-        @endslot
-    @endisset
 
     {{-- Footer --}}
     @slot('footer')
