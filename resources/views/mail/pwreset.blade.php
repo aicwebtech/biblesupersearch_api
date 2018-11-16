4967eeb3b3b0b72dc2e8de357ac3f83fa0e2619a
AIC Web Design <pwm@preservedword.com>
2018-11-15 22:40:02 -0500

    EMAIL FIX - custom view tweak

--- resources/views/mail/pwreset.blade.php
+++ resources/views/mail/pwreset.blade.php
@@ -1,20 +1,20 @@
-<div style='width: 400px; margin: 0 auto'>
+<div style='width: 800px; margin: 0 auto'>
     Bible SuperSearch API<br />
     <br />
     Hello {{ $User->name }} ({{ $User->username }}),<br />
     <br />
     You are receiving this email because we received a password reset request for your account.<br />
     <br />
-    <a href='{{$url}}'>Reset Your Password</a><br />
+    <a href='{{$url}}'>Reset Password</a><br />
     <br />
     If you did not request a password reset, no further action is required.<br />
     <br />
     Regards,<br />
-    Bible SuperSearch API<br />
+    Bible SuperSearch API<br /><br /><br />
 
     <hr />
     <small>
-        If you’re having trouble clicking the "Reset Password" button, copy and paste the URL below into your web browser:
+        If you’re having trouble clicking the "Reset Password" button, please copy and paste the URL below into your web browser:
         <a href='{{$url}}'>{{$url}}</a>
     </small>
 </div>
