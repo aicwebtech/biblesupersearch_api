06695889d705326bb857e4b59f6e40f721199b25
AIC Web Tech <pwm@preservedword.com>
2022-01-05 21:15:33 -0500

    Adding and updating German Bibles

---
+++ app/Models/Verses/Schlachter.php
@@ -0,0 +1,7 @@
+<?php 
+                namespace App\Models\Verses;
+                class Schlachter extends VerseStandard {
+                    protected $hasClass = FALSE;
+                    protected $table = 'verses_schlachter';
+                }
+            
