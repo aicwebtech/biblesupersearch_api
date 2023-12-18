fcca6348fcc7c21914e234f3bc91effd8b732e88
AIC Web Tech <pwm@preservedword.com>
2023-12-17 23:23:02 -0500

    Readying 5.4 release

--- public/js/admin/bibles/package.js
+++ public/js/admin/bibles/package.js
@@ -1,11 +1,8 @@
 enyo.depends(
     //'/js/bin/ckeditor/ckeditor.js',  // v4
     '../../bin/ckeditor5/build/ckeditor.js',
-    //'js/bin/ckeditor5/build/ckeditor.js',
     '../../bin/custom/form',
-    // 'js/bin/custom/form',
     '../../bin/custom/dialog',
-    // 'js/bin/custom/dialog',
     'source',
     'assets/style.css',
     'assets/dialogs.css'
